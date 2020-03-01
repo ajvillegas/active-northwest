fd2c28fa86c8f11f1e4fabe20e828762e0870e07
Alexis J. Villegas <alexis@ajvillegas.com>
2020-02-29 21:22:16 -0800

    Add PHP constants with sample data

---
+++ includes/constants-sample.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file defines the PHP constants.
+ *
+ * @package    AceInTheHole
+ * @author     Alexis J. Villegas
+ * @link       http://www.alexisvillegas.com
+ * @license    GPL-2.0+
+ */
+
+// Database constants.
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'database');
+define('DB_USER', 'user');
+define('DB_PASSWORD', 'password');
