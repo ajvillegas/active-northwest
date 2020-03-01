d74a4ac511ebb55bd83e64aac48eec51209edfd0
Alexis J. Villegas <alexis@ajvillegas.com>
2020-02-29 21:11:52 -0800

    Add volunteer page

---
+++ volunteer.php
@@ -0,0 +1,69 @@
+<?php // phpcs:disable Generic.Files.LineLength ?>
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Volunteering at the annual Ace in the Hole Multisport Weekend is a great way to get involved in the sport you are passionate about while helping others.">
+
+        <title>Volunteer | Ace in the Hole Multisport Event</title>
+
+        <link rel="shortcut icon" href="assets/images/favicon.png">
+        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Montserrat:700%7CSource+Sans+Pro:400,600,700&display=swap">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+
+    <body>
+        <ul class="skip-links">
+            <li>
+                <a class="screen-reader-text" href="#navigation">Skip to navigation</a>
+            </li>
+            <li>
+                <a class="screen-reader-text" href="#content">Skip to content</a>
+            </li>
+            <li>
+                <a class="screen-reader-text" href="#footer">Skip to footer</a>
+            </li>
+        </ul>
+        <?php require 'includes/header.php'; ?>
+
+        <main class="site-inner">
+            <section class="hero-image -volunteer">
+                <div class="wrap">
+                    <h1 class="heading">Become a Volunteer</h1>
+                    <p class="subheading">Get Involved by Joining Our Team</p>
+                </div>
+            </section>
+
+            <section id="content">
+                <div class="wrap">
+                    <h2>Be a Part of Something Great</h2>
+
+                    <p class="lead-text">Volunteering at the annual Ace in the Hole Multisport Weekend is a great way to get involved in the sport you are passionate about while helping others.</p>
+
+                    <p>You will be protecting the safety of our participants, staff and spectators, as well as helping with the organization and logistics of the event. If you are passionate about sports and enjoy helping others while having fun, we would love to hear from you!</p>
+
+                    <p><strong>Here is a list of the various volunteering roles available for you:</strong></p>
+
+                    <ul class="default-ul">
+                        <li><strong>Event Organization</strong> - Ensure that everyone is where they need to be at the correct time.</li>
+                        <li><strong>Staging</strong> - Help all participants get ready on the starting line when it’s time to race.</li>
+                        <li><strong>Chip Timing</strong> - Record the time for each participant after crossing the finish line.</li>
+                    </ul>
+                </div>
+            </section>
+
+            <section class="cta-section -bottom">
+                <div class="wrap">
+                    <p>Interested in volunteering? Click below to contact us and talk about current opportunities.</p>
+
+                    <a href="contact.php" class="button -large">Contact Us</a>
+                </div>
+            </section>
+        </main>
+        <?php require 'includes/footer.php'; ?>
+    </body>
+
+</html>
