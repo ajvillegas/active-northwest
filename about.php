d91c46a6a301428e3436088165007e1ef01587d2
Alexis J. Villegas <alexis@ajvillegas.com>
2020-03-16 17:24:13 -0700

    Add about page

---
+++ about.php
@@ -0,0 +1,278 @@
+<?php // phpcs:disable Generic.Files.LineLength ?>
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Learn more about Ace in the Hole Multisport Event weekend of 2020 with events for athletes of all shapes, sizes and physical abilities.">
+
+        <title>About | Ace in the Hole Multisport Event</title>
+
+        <link rel="shortcut icon" href="assets/images/favicon.png">
+        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Montserrat:700%7CSource+Sans+Pro:400,600,700&display=swap">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+
+    <body>
+        <ul class="skip-links">
+            <li>
+                <a class="screen-reader-text" href="#navigation">Skip to navigation</a>
+            </li>
+            <li>
+                <a class="screen-reader-text" href="#content">Skip to content</a>
+            </li>
+            <li>
+                <a class="screen-reader-text" href="#itinerary">Skip to itinerary</a>
+            </li>
+            <li>
+                <a class="screen-reader-text" href="#course">Skip to course details</a>
+            </li>
+            <li>
+                <a class="screen-reader-text" href="#equipment">Skip to equipment section</a>
+            </li>
+            <li>
+                <a class="screen-reader-text" href="#faqs">Skip to FAQs</a>
+            </li>
+            <li>
+                <a class="screen-reader-text" href="#footer">Skip to footer</a>
+            </li>
+        </ul>
+        <?php require 'includes/header.php'; ?>
+
+        <main class="site-inner">
+            <section class="hero-image -about">
+                <div class="wrap">
+                    <h1 class="heading">About The Event</h1>
+                    <p class="subheading">Learn More About Our Action-Packed Weekend</p>
+                </div>
+            </section>
+
+            <section id="content">
+                <div class="wrap">
+                    <h2>AITH Multisport Weekend Event Information</h2>
+
+                    <p class="lead-text">Ace in the Hole Multisport Event is proud to offer running and triathlon events to athletes of all shapes and sizes, national origins, gender identifications and cultural backgrounds. We offer Events for every body.</p>
+
+                    <p>The Annual Ace in the Hole Multisport Weekend is a legendary event in the Oregon triathlon and running community. It has become a traditional destination race for athletes from across the nation.</p>
+
+                    <p>There is something for every level of athletic ability. The action-packed weekend includes a first timer triathlon, a sprint and olympic triathlons, a half-iron triathlon, 10K and half marathon runs. Wether you come to experience your first race or come ready to win, we're sure you'll have fun!</p>
+
+                    <hr>
+                </div>
+            </section>
+
+            <section id="itinerary">
+                <div class="wrap">
+                    <h3>Itinerary</h3>
+
+                    <div class="table-wrap">
+                        <table class="min-500">
+                            <tr>
+                                <th>Event</th>
+                                <th>Day</th>
+                                <th>Time</th>
+                            </tr>
+                            <tr>
+                                <td>Long Course Triathlon</td>
+                                <td>Saturday</td>
+                                <td>7:00 AM</td>
+                            </tr>
+                            <tr>
+                                <td>Olympic Triathlon</td>
+                                <td>Saturday</td>
+                                <td>7:30 AM</td>
+                            </tr>
+                            <tr>
+                                <td>10K</td>
+                                <td>Saturday</td>
+                                <td>7:15 AM</td>
+                            </tr>
+                            <tr>
+                                <td>Half Marathon</td>
+                                <td>Saturday</td>
+                                <td>7:15 AM</td>
+                            </tr>
+                            <tr>
+                                <td>Sprint Triathlon</td>
+                                <td>Sunday</td>
+                                <td>8:00 AM</td>
+                            </tr>
+                            <tr>
+                                <td>Try-a-Tri</td>
+                                <td>Sunday</td>
+                                <td>8:20 AM</td>
+                            </tr>
+                            <tr>
+                                <td>Splash n Dash</td>
+                                <td>Sunday</td>
+                                <td>12:00 PM</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </section>
+
+            <section id="course">
+                <div class="wrap">
+                    <h3>Course Details</h3>
+
+                    <div class="content-box">
+                        <p>Water temperature is expected to be between 62 & 66 degrees. The temperature will be taken on Friday and the morning of the race. <strong>Wetsuits are recommended.</strong></p>
+                    </div>
+
+                    <hr>
+
+                    <h4>Long Course <em>Swim</em> (1.2 miles)</h4>
+
+                    <p>Participants will make two counter-clockwise loops. Large buoys will mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support will be present on the beach.</p>
+
+                    <h4>Long Course <em>Bike</em> (58 miles)</h4>
+
+                    <p>A scenic point-to-point course that travels over gently rolling hills prior to three hard climbs. The bike course will be marked with large directional signage and there will be course marshals at key intersections to help direct you.</p>
+
+                    <h4>Long Course <em>Run</em> (13.1 miles)</h4>
+
+                    <p>A mostly flat loop course on widely paved bike paths that traverse through and around this beautiful and scenic destination resort (two hills total with a minimal elevation gain.</p>
+
+                    <hr>
+
+                    <h4>Olympic <em>Swim</em> (1,500 meters)</h4>
+
+                    <p>Participants will make two counter-clockwise loops. Large buoys will mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support will be present on the beach.</p>
+
+                    <h4>Olympic <em>Bike</em> (28 miles)</h4>
+
+                    <p> A scenic point-to-point course that travels over gently rolling hills. The bike course will be marked with large directional signage and course marshals will be present at key intersections.</p>
+
+                    <h4>Olympic <em>Run</em> (10K)</h4>
+
+                    <p>A mostly flat loop course on widely paved bike paths that traverse through and around this beautiful and scenic destination resort (one hill total with a minimal elevation gain).</p>
+
+                    <hr>
+
+                    <h4>Sprint</h4>
+
+                    <p>Course will offer a 1-loop 1/2 mile swim, the exact same 28 mile Bike Course as the Olympic distance and a 5km run.</p>
+
+                    <hr>
+
+                    <h4>Try-a-Tri</h4>
+
+                    <p>This novice race is designed for the first time triathlete, those new to the sport, our Junior Triathletes. The swim is a shorter, more manageable 1/4 mile distance, (instead of the standard 1/2-mile Sprint distance swim), 10 mile bike ride (vs 12 miles and it's a 2 loop course making it very spectator friendly!) and a flat 2 mile run (vs 3 mile sprint course).</p>
+
+                    <hr>
+
+                    <h4>Half Marathon Course</h4>
+
+                    <p>Half-Marathon (13.1-miles): Half-Marathon event starts and finishes in the Athletes Village to the cheers of the enthusiastic crowd. Once finished, runners can enjoy the finish line festivities, including the Sports & Fitness Expo and live entertainment.</p>
+
+                    <p>Post-race refreshments will be provided and the Awards Ceremony for the Half-Marathon will begin once the results have been certified.</p>
+
+                    <hr>
+
+                    <h4>10k Course</h4>
+
+                    <p>The 10K event starts and finishes in the Athletes Village. The paths are approximately 6 ft wide, perfectly paved and wind around through the forest. Each course has only two small hills with a minimal elevation gain to navigate and a fast downhill to flat finish to the roaring cheers of the crowd.</p>
+
+                    <hr>
+
+                    <h4>Splash n Dash</h4>
+
+                    <p>Kids event for participants ages 4 to 15. This is a 100 yard swim and a 50 yard dash. It is free if an adult <a href="register.php">registers</a> for an event otherwise it is $25.</p>
+
+                    <hr>
+                </div>
+            </section>
+
+            <section id="equipment">
+                <div class="wrap">
+                    <h3>What to Bring</h3>
+
+                    <p>Watch the weather closely. The show goes on no matter what the weather is doing.</p>
+
+                    <p>You must wear your chip timing piece during the entire event. You will be given a band that will hold your chip timing piece around your ankle throughout the entire event. Be sure that it is snapped tightly.</p>
+
+                    <p>Be sure to have your Chip Timing piece on before you start the race and be sure to step over the timing mats after each segment of the race. If you are wearing a wet-suit, make sure the timing piece goes under your wetsuit otherwise, you will have a very difficult time getting off your wet suit.</p>
+
+                    <h4>Swim</h4>
+
+                    <p>Wetsuits are optional for the swim but will provide buoyancy and warmth. However, many people opt for no wetsuit for a triathlon so no worries. We will provide you with a swim cap but you will want to bring your own goggles.</p>
+
+                    <h4>Bike</h4>
+
+                    <p>A biking helmet is mandatory. You will also receive 2 stickers in your package with your race number on them. The small sticker should go on the front of your helmet. The bigger sticker will wrap around your bike frame. Road or mountain bikes are acceptable. For safety reasons, be sure to have end-caps on the end of your handle-bars.</p>
+
+                    <h4>Run</h4>
+
+                    <p>You must finish the race with your bib number on the front of you. Some people choose to pin it on at the beginning of the race and have it on for the whole event so they don't have to worry about it.</p>
+
+                    <p>Others pin it on a singlet that they put on once they finish the swim before they head out for the bike. Others use an elastic racing strap that they pin their bib number to and then quickly strap it on before they leave for the run. Use whatever option feels best for you.<p>
+
+                    <p>Remember to bring a change of clothing so you can enjoy the post-event festivities.</p>
+
+                    <hr>
+                </div>
+            </section>
+
+            <section id="faqs">
+                <div class="wrap">
+                    <h3>Frequently Asked Questions</h3>
+
+                    <details>
+                        <summary>What are the Rules?</summary>
+
+                        <div class="answer">
+                            <p>We currently adhere to the USAT Rules for Triathlon & Duathlon. Important rules include no drafting, you must wear a helmet and music is ABSOLUTELY NOT allowed during the bike for obvious safety reasons.</p>
+                        </div>
+                    </details>
+
+                    <details>
+                        <summary>Can I use a personal music device while cycling?</summary>
+
+                        <div class="answer">
+                            <p>Absolutely no music devices are allowed during the bike segment for obvious safety risks and will result in immediate disqualification.</p>
+                        </div>
+                    </details>
+
+                    <details>
+                        <summary>Can I use a personal music device while running?</summary>
+
+                        <div class="answer">
+                            <p>During any running segments, we prefer for athletes to NOT use music devices to maximize safety, assure a fair competitive environment and assure athletes can hear all course marshal instructions.</p>
+
+                            <p>With that said, we also understand that many athletes rely on music to help endure the challenge of running. To accommodate the needs of our athletes while still assuring maximum safety and a fair, competitive environment, here is the arrangement that we can accommodate:</p>
+
+                            <ul class="default-ul">
+                                <li>If you chose to use a personal music device, you must always keep one ear open for instructions so only one ear-bud can be used at any time. In addition, if you opt to use music, you will not be eligible for awards, points and rankings. So as an athlete, you have to make a choice - music or awards/points/rankings.</li>
+
+                                <li>If you are using a personal music devise with two earbuds, you will be immediately disqualified. Trust us, we don't want to have to enforce a disqualification but in order to assure the safety of all athletes, this is a very important rule.</li>
+
+                                <li>We also request that if you chose to use a personal music device that you keep the volume to a minimum to assure all instructions can be heard.</li>
+                            </ul>
+                        </div>
+                    </details>
+
+                    <details>
+                        <summary>Do I need to wear a wetsuit?</summary>
+
+                        <div class="answer">
+                            <p>No, you do not need to wear a wetsuit. Many will not wear a wetsuit while others will choose to wear a wetsuit because of the buoyancy and warmth factor.</p>
+                        </div>
+                    </details>
+
+                    <details>
+                        <summary>Do I have to use a road or racing bike?</summary>
+
+                        <div class="answer">
+                            <p>No. We welcome any type of bike as long as it is functioning properly with brakes and endcaps at the end of your handlebars.</p>
+                        </div>
+                    </details>
+                </div>
+            </section>
+        </main>
+        <?php require 'includes/footer.php'; ?>
+    </body>
+
+</html>
