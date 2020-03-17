fd06ba736772d4217ed8a964604573e35dc518f8
Alexis J. Villegas <alexis@ajvillegas.com>
2020-03-16 17:35:11 -0700

    Update social media links and add weather widget

--- includes/header.php
+++ includes/header.php
@@ -5,12 +5,12 @@
                 <div class="social">
                     <ul class="horizontal">
                         <li class="facebook">
-                            <a href="http://www.facebook.com" title="Facebook">
+                            <a href="https://www.facebook.com/Cas222Aceinthehole-110661963841617/" title="Facebook">
                                 <span class="screen-reader-text">Follow Us on Facebook</span>
                             </a>
                         </li>
                         <li class="twitter">
-                            <a href="http://www.twitter.com" title="Twitter">
+                            <a href="https://twitter.com/pcccas222" title="Twitter">
                                 <span class="screen-reader-text">Follow Us on Twitter</span>
                             </a>
                         </li>
@@ -29,32 +29,177 @@
                     </a>
                 </div>
 
-                <nav id="navigation" class="nav-menu" aria-label="primary">
-                    <button class="menutoggle" aria-haspopup="true" aria-expanded="false" data-menu-id="primary-menu">
-                        <span class="navicon"></span>
-                        <span class="screen-reader-text">Menu</span>
-                    </button>
+                <div class="nav-wrap">
+                    <nav id="navigation" class="nav-menu" aria-label="primary">
+                        <button class="menutoggle" aria-haspopup="true" aria-expanded="false" data-menu-id="primary-menu">
+                            <span class="navicon"></span>
+                            <span class="screen-reader-text">Menu</span>
+                        </button>
 
-                    <ul id="primary-menu">
-                        <li>
-                            <a href="index.php" class="navlink"><span>Home</span></a>
-                        </li>
-                        <li>
-                            <a href="#" class="navlink"><span>About</span></a>
-                        </li>
-                        <li>
-                            <a href="register.php" class="navlink"><span>Register</span></a>
-                        </li>
-                        <li>
-                            <a href="volunteer.php" class="navlink"><span>Volunteer</span></a>
-                        </li>
-                        <li>
-                            <a href="#" class="navlink"><span>Gallery</span></a>
-                        </li>
-                        <li>
-                            <a href="contact.php" class="navlink"><span>Contact</span></a>
-                        </li>
-                    </ul>
-                </nav>
+                        <ul id="primary-menu">
+                            <li>
+                                <a href="index.php" class="navlink"><span>Home</span></a>
+                            </li>
+                            <li>
+                                <a href="about.php" class="navlink"><span>About</span></a>
+                            </li>
+                            <li>
+                                <a href="register.php" class="navlink"><span>Register</span></a>
+                            </li>
+                            <li>
+                                <a href="volunteer.php" class="navlink"><span>Volunteer</span></a>
+                            </li>
+                            <li>
+                                <a href="gallery.php" class="navlink"><span>Gallery</span></a>
+                            </li>
+                            <li>
+                                <a href="contact.php" class="navlink"><span>Contact</span></a>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <div class="weather-widget">
+                        <button class="weathertoggle" aria-haspopup="true" aria-expanded="false" title="Weather Forecast">
+                            <span class="weathericon"></span>
+                            <span class="screen-reader-text">Portland, Oregon Weather Widget</span>
+                        </button>
+
+                        <div id="weather" class="weather">
+                            <div class="wrap">
+                                <div class="weatherinfo">
+                                    <div class="current">
+                                        <h2 class="location">
+                                            <span class="heading">Portland</span>
+                                            <span class="subheading">Weather</span>
+                                        </h2>
+
+                                        <div class="currenticon">
+                                            <img src="assets/images/icon/clear-day.svg" alt="Weather Condition Icon" title="Weather summary.">
+                                        </div>
+
+                                        <div class="conditions">
+                                            <div class="temp">0°F</div>
+                                            <div class="desc">Conditions</div>
+                                        </div>
+                                    </div>
+
+                                    <div class="forecast">
+                                        <div class="day">
+                                            <div class="name">
+                                                <span class="fullname">Monday</span>
+                                                <span class="abbrname">Mon</span>
+                                            </div>
+
+                                            <div class="dayicon">
+                                                <img src="assets/images/icon/clear-day.svg" alt="Weather Condition Icon" title="Weather summary.">
+                                            </div>
+
+                                            <div class="temps">
+                                                <div class="tempmax">0°F</div>
+                                                <div class="tempmin">0°F</div>
+                                            </div>
+                                        </div>
+
+                                        <div class="day">
+                                            <div class="name">
+                                                <span class="fullname">Tuesday</span>
+                                                <span class="abbrname">Tue</span>
+                                            </div>
+
+                                            <div class="dayicon">
+                                                <img src="assets/images/icon/rain.svg" alt="Weather Condition Icon" title="Weather summary.">
+                                            </div>
+
+                                            <div class="temps">
+                                                <div class="tempmax">0°F</div>
+                                                <div class="tempmin">0°F</div>
+                                            </div>
+                                        </div>
+
+                                        <div class="day">
+                                            <div class="name">
+                                                <span class="fullname">Wednesday</span>
+                                                <span class="abbrname">Wed</span>
+                                            </div>
+
+                                            <div class="dayicon">
+                                                <img src="assets/images/icon/partly-cloudy-day.svg" alt="Weather Condition Icon" title="Weather summary.">
+                                            </div>
+
+                                            <div class="temps">
+                                                <div class="tempmax">0°F</div>
+                                                <div class="tempmin">0°F</div>
+                                            </div>
+                                        </div>
+
+                                        <div class="day">
+                                            <div class="name">
+                                                <span class="fullname">Thursday</span>
+                                                <span class="abbrname">Thu</span>
+                                            </div>
+
+                                            <div class="dayicon">
+                                                <img src="assets/images/icon/partly-cloudy-day.svg" alt="Weather Condition Icon" title="Weather summary.">
+                                            </div>
+
+                                            <div class="temps">
+                                                <div class="tempmax">0°F</div>
+                                                <div class="tempmin">0°F</div>
+                                            </div>
+                                        </div>
+
+                                        <div class="day">
+                                            <div class="name">
+                                                <span class="fullname">Friday</span>
+                                                <span class="abbrname">Fri</span>
+                                            </div>
+
+                                            <div class="dayicon">
+                                                <img src="assets/images/icon/rain.svg" alt="Weather Condition Icon" title="Weather summary.">
+                                            </div>
+
+                                            <div class="temps">
+                                                <div class="tempmax">0°F</div>
+                                                <div class="tempmin">0°F</div>
+                                            </div>
+                                        </div>
+
+                                        <div class="day">
+                                            <div class="name">
+                                                <span class="fullname">Saturday</span>
+                                                <span class="abbrname">Sat</span>
+                                            </div>
+
+                                            <div class="dayicon">
+                                                <img src="assets/images/icon/clear-day.svg" alt="Weather Condition Icon" title="Weather summary.">
+                                            </div>
+
+                                            <div class="temps">
+                                                <div class="tempmax">0°F</div>
+                                                <div class="tempmin">0°F</div>
+                                            </div>
+                                        </div>
+
+                                        <div class="day">
+                                            <div class="name">
+                                                <span class="fullname">Sunday</span>
+                                                <span class="abbrname">Sun</span>
+                                            </div>
+
+                                            <div class="dayicon">
+                                                <img src="assets/images/icon/partly-cloudy-day.svg" alt="Weather Condition Icon" title="Weather summary.">
+                                            </div>
+
+                                            <div class="temps">
+                                                <div class="tempmax">0°F</div>
+                                                <div class="tempmin">0°F</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </header>
