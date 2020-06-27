70945b5b7cf4399ce014a41b620d60fa68840e9b
Alexis J. Villegas <alexis@ajvillegas.com>
2020-06-26 21:27:47 -0700

    Update markup and relative URLs

--- includes/footer.php
+++ includes/footer.php
@@ -13,18 +13,18 @@
                     </div>
 
                     <div class="phone">
-                        <a href="tel:9715552484">(971) 555-AITH</a>
+                        <a href="tel:9715552484">(971) 555-2484</a>
                     </div>
 
                     <div class="social">
                         <ul class="horizontal">
                             <li class="facebook">
-                                <a href="https://www.facebook.com/Cas222Aceinthehole-110661963841617/" title="Facebook">
+                                <a href="https://www.facebook.com/" title="Facebook">
                                     <span class="screen-reader-text">Follow Us on Facebook</span>
                                 </a>
                             </li>
                             <li class="twitter">
-                                <a href="https://twitter.com/pcccas222" title="Twitter">
+                                <a href="https://twitter.com/" title="Twitter">
                                     <span class="screen-reader-text">Follow Us on Twitter</span>
                                 </a>
                             </li>
@@ -38,22 +38,22 @@
                     <nav aria-label="footer">
                         <ul>
                             <li>
-                                <a href="index.php"><span>Home</span></a>
+                                <a href="/"><span>Home</span></a>
                             </li>
                             <li>
-                                <a href="register.php"><span>Register</span></a>
+                                <a href="/register/"><span>Register</span></a>
                             </li>
                             <li>
-                                <a href="gallery.php"><span>Gallery</span></a>
+                                <a href="/gallery/"><span>Gallery</span></a>
                             </li>
                             <li>
-                                <a href="about.php"><span>About</span></a>
+                                <a href="/about/"><span>About</span></a>
                             </li>
                             <li>
-                                <a href="volunteer.php"><span>Volunteer</span></a>
+                                <a href="/volunteer/"><span>Volunteer</span></a>
                             </li>
                             <li>
-                                <a href="contact.php"><span>Contact</span></a>
+                                <a href="/contact/"><span>Contact</span></a>
                             </li>
                         </ul>
                     </nav>
@@ -68,7 +68,7 @@
 
             <div class="credits">
                 <p>
-                    Copyright &copy; <span id="year">2020</span> Ace in the Hole <br><span>|</span> Website by
+                    Copyright &copy; <span id="year">2020</span> Active Northwest <br><span>|</span> Website by
                     <a href="http://www.alexisvillegas.com/">AJV</a>
                 </p>
 
@@ -82,4 +82,4 @@
         </button>
 
         <script src="//maps.google.com/maps/api/js?key=AIzaSyDZXlksw2XKFvmh3i3Zw7NDzjfgir9N8DI"></script>
-        <script src="assets/js/custom.min.js"></script>
+        <script src="<?php echo $base_url; ?>assets/js/custom.min.js"></script>
