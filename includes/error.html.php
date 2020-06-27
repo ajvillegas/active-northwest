4d2f43130e7d95eb5dd297a028778abeef294d8d
Alexis J. Villegas <alexis@ajvillegas.com>
2020-06-26 21:27:00 -0700

    Update relative URLs

--- includes/error.html.php
+++ includes/error.html.php
@@ -8,26 +8,26 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="There was an error with the PHP script.">
 
-        <title>Script Error | Ace in the Hole Multisport Event</title>
+        <title>Script Error | Active Northwest Multisport Event</title>
 
-        <link rel="shortcut icon" href="assets/images/favicon.png">
+        <link rel="shortcut icon" href="../assets/images/favicon.png">
         <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Montserrat:700%7CSource+Sans+Pro:400,600,700&display=swap">
-        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="../assets/css/style.css">
     </head>
 
     <body>
         <ul class="skip-links">
             <li>
-                <a class="screen-reader-text" href="#navigation">Skip to navigation</a>
+                <a class="screen-reader-text" href="/#navigation">Skip to navigation</a>
             </li>
             <li>
-                <a class="screen-reader-text" href="#content">Skip to content</a>
+                <a class="screen-reader-text" href="/#content">Skip to content</a>
             </li>
             <li>
-                <a class="screen-reader-text" href="#footer">Skip to footer</a>
+                <a class="screen-reader-text" href="/#footer">Skip to footer</a>
             </li>
         </ul>
-        <?php require 'includes/header.php'; ?>
+        <?php require '../includes/header.php'; ?>
 
         <main class="site-inner">
             <section class="hero-image">
@@ -43,11 +43,11 @@
                 </div>
             </section>
         </main>
-        <?php require 'includes/footer.php'; ?>
+        <?php require '../includes/footer.php'; ?>
         <script>
             // Remove $_POST parameters from URL.
             ( function() {
-                window.history.replaceState({}, document.title, '/cas222/project/<?php echo basename($_SERVER['PHP_SELF']); ?>' );
+                window.history.replaceState({}, document.title, '<?php echo $_SERVER['REQUEST_URI']; ?>' );
             }() );
         </script>
     </body>
