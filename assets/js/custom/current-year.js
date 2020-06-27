631848208c593e3342220e1c7303712bbf8c6701
Alexis J. Villegas <alexis@ajvillegas.com>
2020-06-26 21:21:12 -0700

    Rename select element variable

--- assets/js/custom/current-year.js
+++ assets/js/custom/current-year.js
@@ -1,17 +1,17 @@
 /**
  * Calculate the current year and update it in the footer.
  *
- * @package    AceInTheHole
+ * @package    ActiveNorthwest
  * @author     Alexis J. Villegas
  * @link       http://www.alexisvillegas.com
  * @license    GPL-2.0+
  */
 
 ( function() {
-	const element = document.querySelector( '#year' );
+	const footerYear = document.querySelector( '#year' );
 	const currentYear = new Date().getFullYear();
 
-	if ( element && ( currentYear !== element.innerHTML ) ) {
-		element.innerHTML = currentYear;
+	if ( footerYear && ( currentYear !== footerYear.innerText ) ) {
+		footerYear.innerText = currentYear;
 	}
 }() );
