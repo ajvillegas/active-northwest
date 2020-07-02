59ec8513795b7246df2e0f37ed32defb69af296d
Alexis J. Villegas <alexis@ajvillegas.com>
2020-07-02 02:24:29 -0700

    Update alt image text

--- gallery/index.php
+++ gallery/index.php
@@ -42,103 +42,103 @@
                     <div class="masonry-gallery">
                         <figure class="brick">
                             <a href="../assets/images/gallery/ace2.jpg">
-                                <img src="../assets/images/gallery/ace2.jpg" alt="Past Event Participants">
+                                <img src="../assets/images/gallery/ace2.jpg" alt="Older woman walks on marathon competition with the help of crutches.">
                             </a>
                         </figure>
 
                         <figure class="brick">
                             <a href="../assets/images/gallery/ace1.jpg">
-                                <img src="../assets/images/gallery/ace1.jpg" alt="Past Event Participants">
+                                <img src="../assets/images/gallery/ace1.jpg" alt="Group of triathlon competitors rush to the water to begin the swiming portion.">
                             </a>
                         </figure>
 
                         <figure class="brick">
                             <a href="../assets/images/gallery/ace3.jpg">
-                                <img src="../assets/images/gallery/ace3.jpg" alt="Past Event Participants">
+                                <img src="../assets/images/gallery/ace3.jpg" alt="A cyclist celebrates his approach to the finish line with extended arms in front of the other competitors.">
                             </a>
                         </figure>
 
                         <figure class="brick">
                             <a href="../assets/images/gallery/ace4.jpg">
-                                <img src="../assets/images/gallery/ace4.jpg" alt="Past Event Participants">
+                                <img src="../assets/images/gallery/ace4.jpg" alt="Group of swimmers in wetsuits run to the water to begin the swimming race.">
                             </a>
                         </figure>
 
                         <figure class="brick">
                             <a href="../assets/images/gallery/ace5.jpg">
-                                <img src="../assets/images/gallery/ace5.jpg" alt="Past Event Participants">
+                                <img src="../assets/images/gallery/ace5.jpg" alt="Cyclists ride in front of the sun on a clear day with blue skies.">
                             </a>
                         </figure>
 
                         <figure class="brick">
                             <a href="../assets/images/gallery/ace6.jpg">
-                                <img src="../assets/images/gallery/ace6.jpg" alt="Past Event Participants">
+                                <img src="../assets/images/gallery/ace6.jpg" alt="Three triathlon competitors pose together for the camera on a beach.">
                             </a>
                         </figure>
 
                         <figure class="brick">
                             <a href="../assets/images/gallery/ace7.jpg">
-                                <img src="../assets/images/gallery/ace7.jpg" alt="Past Event Participants">
+                                <img src="../assets/images/gallery/ace7.jpg" alt="A female runner high-fives expectators as she approaches the finish line.">
                             </a>
                         </figure>
 
                         <figure class="brick">
                             <a href="../assets/images/gallery/ace8.jpg">
-                                <img src="../assets/images/gallery/ace8.jpg" alt="Past Event Participants">
+                                <img src="../assets/images/gallery/ace8.jpg" alt="Woman running on a dirt road ahead of the competition pack smiling for the camera.">
                             </a>
                         </figure>
 
                         <figure class="brick">
                             <a href="../assets/images/gallery/ace9.jpg">
-                                <img src="../assets/images/gallery/ace9.jpg" alt="Past Event Participants">
+                                <img src="../assets/images/gallery/ace9.jpg" alt="A group of competitors on racing wheelchairs approach the finish line.">
                             </a>
                         </figure>
 
                         <figure class="brick">
                             <a href="../assets/images/gallery/ace10.jpg">
-                                <img src="../assets/images/gallery/ace10.jpg" alt="Past Event Participants">
+                                <img src="../assets/images/gallery/ace10.jpg" alt="A runner crossing the finish line first celebrates with extended arms.">
                             </a>
                         </figure>
 
                         <figure class="brick">
                             <a href="../assets/images/gallery/ace11.jpg">
-                                <img src="../assets/images/gallery/ace11.jpg" alt="Past Event Participants">
+                                <img src="../assets/images/gallery/ace11.jpg" alt="A camera shot of runners on a race from the waist down.">
                             </a>
                         </figure>
 
                         <figure class="brick">
                             <a href="../assets/images/gallery/ace12.jpg">
-                                <img src="../assets/images/gallery/ace12.jpg" alt="Past Event Participants">
+                                <img src="../assets/images/gallery/ace12.jpg" alt="A group of racing cyclists powering through a corner.">
                             </a>
                         </figure>
 
                         <figure class="brick">
                             <a href="../assets/images/gallery/ace13.jpg">
-                                <img src="../assets/images/gallery/ace13.jpg" alt="Past Event Participants">
+                                <img src="../assets/images/gallery/ace13.jpg" alt="A group of young running competitors wearing jerseys begin to race.">
                             </a>
                         </figure>
 
                         <figure class="brick">
                             <a href="../assets/images/gallery/ace14.jpg">
-                                <img src="../assets/images/gallery/ace14.jpg" alt="Past Event Participants">
+                                <img src="../assets/images/gallery/ace14.jpg" alt="Female track and field competitors race on a track.">
                             </a>
                         </figure>
 
                         <figure class="brick">
                             <a href="../assets/images/gallery/ace15.jpg">
-                                <img src="../assets/images/gallery/ace15.jpg" alt="Past Event Participants">
+                                <img src="../assets/images/gallery/ace15.jpg" alt="A fit older man running a marathon wearing a white sports hat and jersey.">
                             </a>
                         </figure>
 
                         <figure class="brick">
                             <a href="../assets/images/gallery/ace16.jpg">
-                                <img src="../assets/images/gallery/ace16.jpg" alt="Past Event Participants">
+                                <img src="../assets/images/gallery/ace16.jpg" alt="A large group of runners wearing orange long-sleeve jerseys move through a corner in a race.">
                             </a>
                         </figure>
 
                         <figure class="brick">
                             <a href="../assets/images/gallery/ace17.jpg">
-                                <img src="../assets/images/gallery/ace17.jpg" alt="Past Event Participants">
+                                <img src="../assets/images/gallery/ace17.jpg" alt="Marathon runners exiting a road tunnel viewed from the top street level.">
                             </a>
                         </figure>
                     </div>
